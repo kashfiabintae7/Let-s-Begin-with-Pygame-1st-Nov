50c6479ae631d3f60221d993f8ab07a4f5a3eddc
Kashfia <kashfiabintae@gmail.com>
2024-11-08 18:16:34 +0600

    first commit

---
+++ Pygame window.py
@@ -0,0 +1,20 @@
+import pygame 
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 300))
+color = (247, 79, 198)
+
+screen.fill(color)
+
+done = False
+
+while not done:
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            
+    pygame.display.flip()
+    
